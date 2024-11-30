0ba5bc3b503136751c088ae8e9586def243083f5
sufyan <sufyan.mumtaz@solutionsloft.com>
2024-11-30 16:28:35 +0500

    upadte

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,12 @@
 // routes/authRoutes.js
-const express = require('express');
-const { login } = require('../controllers/auth');
+const express = require("express");
+const { login } = require("../controllers/auth");
+const { messaging } = require("firebase-admin");
 const router = express.Router();
 
-router.post('/login', login);
+router.get("/", (req, res) => {
+  res.status(200).send({ message: "Server is working" });
+});
+router.post("/login", login);
 
 module.exports = router;
