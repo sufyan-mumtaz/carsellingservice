0fd33996f62b27affc509ba6d1286290a7e425d7
sufyan <sufyan.mumtaz@solutionsloft.com>
2024-11-30 23:22:16 +0500

    update

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,6 @@
 // routes/authRoutes.js
 const express = require("express");
 const { login } = require("../controllers/auth");
-const { messaging } = require("firebase-admin");
 const router = express.Router();
 
 router.get("/", (req, res) => {
